c8ffeea73c8ad2da0cdef95a3a9297d33c76565f
Arvind Deshraj <arvind.deshraj@gmail.com>
2018-04-05 14:58:00 +0530

    Added the date form. Takes input in dd-mm-yyyy format

--- pollution_data/forms.py
+++ pollution_data/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class DateForm(forms.Form):
-    created_on = forms.DateField()
+    created_on = forms.DateField(input_formats=('%d-%m-%Y',),widget=forms.widgets.DateInput(format="%d-%m-%Y"))
 
     class Meta:
         fields = '__all__'
--- pollution_data/views.py
+++ pollution_data/views.py
@@ -21,5 +21,18 @@ def showMap(request):
     # if request.GET['date']:
     #     date = request.GET['date']
 
-    form = DateForm()
-    return render(request,"pms/Dashboard_mist/pages/maps.html",{'f':form})
+    # form = DateForm()
+    # return render(request,"pms/Dashboard_mist/pages/maps.html",{'f':form})
+    if request.method == 'POST':
+        form = DateForm(request.POST)
+
+        if form.is_valid():
+            datevalue = form.cleaned_data['created_on']
+            all_data = Data.objects.filter(created_on__date = datevalue)
+            print(all_data)
+            return render(request,"pms/Dashboard_mist/pages/maps.html", {'f':form, 'dt':datevalue, 'pd':all_data} )
+        else:
+            print("The format of the date is bricked!!!")
+    else:
+        form = DateForm()
+        return render(request, "pms/Dashboard_mist/pages/maps.html",{'f':form})
